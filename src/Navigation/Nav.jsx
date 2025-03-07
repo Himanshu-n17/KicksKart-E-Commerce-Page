a328a1cbbe5552efd56d753630a64c4c7a0464a4
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-08 02:48:49 +0530

    KicksKart

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,3 +1,5 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Nav.css";
 const nav = () => {
   return (
@@ -8,6 +10,18 @@ const nav = () => {
           className="search-input"
           placeholder="Enter Your Preferred Shoes"
         />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
       </div>
     </nav>
   );
