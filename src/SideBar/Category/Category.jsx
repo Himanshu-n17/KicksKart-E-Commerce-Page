eac66494b60cea68d9f4508395f5c6e429b21b89
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-08 10:31:50 +0530

    Side Bar

---
+++ src/SideBar/Category/Category.jsx
@@ -0,0 +1,22 @@
+import "./Category.css";
+
+const Category = () => {
+  return (
+    <div>
+      <h2 className="sidebar-title">Category</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <input value="sneakers" title="Sneakers" name="test" />
+        <input value="flats" title="Flats" name="test" />
+        <input value="sandals" title="Sandals" name="test" />
+        <input value="heels" title="Heels" name="test" />
+      </div>
+    </div>
+  );
+};
+
+export default Category;
