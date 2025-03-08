0f4c4ccce80db4fdafd562c1a2c10b8a915d12dd
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-08 10:15:44 +0530

    Recommended

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,7 +1,29 @@
 import "./Recommended.css";
 
 const Recommended = () => {
-  return <div>Recommended</div>;
+  return (
+    <>
+      <div>
+        <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-flex">
+          <button value="abcd" title="All Products">
+            All Products
+          </button>
+          <button value="Nike" title="Nike">
+            Nike
+          </button>
+          <button value="Adidas" title="Adidas">
+            Adidas
+          </button>
+          <button value="Puma" title="Puma">
+            Puma
+          </button>
+          <button value="Vans" title="Vans">
+            Vans
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
-
 export default Recommended;
