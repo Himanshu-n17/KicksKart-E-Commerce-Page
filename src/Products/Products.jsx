533fe0332b97de1739c2b17474f89e0a10ed7397
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-12 23:50:37 +0530

    Products

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -15,10 +15,10 @@ const Products = () => {
           <div className="card-details">
             <h3 className="card-title">Shoe</h3>
             <section className="card-reviews">
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
               <span className="total-reviews">4</span>
             </section>
             <section className="card-price">
