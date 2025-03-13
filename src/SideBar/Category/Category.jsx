894aaaa4ed7b566b4fe491c27e4b5b06d76a1966
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-13 21:59:37 +0530

    SideBar

--- src/SideBar/Category/Category.jsx
+++ src/SideBar/Category/Category.jsx
@@ -3,18 +3,30 @@ import "./Category.css";
 const Category = () => {
   return (
     <div>
-      {/* <h2 className="sidebar-title">Category</h2>
+      <h2 className="sidebar-title">Category</h2>
 
       <div>
         <label className="sidebar-label-container">
           <input type="radio" value="" name="test" />
           <span className="checkmark"></span>All
         </label>
-        <input value="sneakers" title="Sneakers" name="test" />
-        <input value="flats" title="Flats" name="test" />
-        <input value="sandals" title="Sandals" name="test" />
-        <input value="heels" title="Heels" name="test" />
-      </div> */}
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
     </div>
   );
 };
