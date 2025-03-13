229d41695fed9942d78ce3b3cba40af732b191d3
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-13 21:55:16 +0530

    Products

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,115 +1,14 @@
-import { AiFillStar } from "react-icons/ai";
-import { FaShoppingBag } from "react-icons/fa";
+import Card from "../Components/Card";
 import "./Products.css";
 
 const Products = () => {
   return (
     <>
       <section className="card-container">
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-reviews">
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-
-              <div className="bag">
-                <FaShoppingBag />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-reviews">
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-
-              <div className="bag">
-                <FaShoppingBag />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-reviews">
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-
-              <div className="bag">
-                <FaShoppingBag />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-reviews">
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <AiFillStar className="rating-star" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-
-              <div className="bag">
-                <FaShoppingBag />
-              </div>
-            </section>
-          </div>
-        </section>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </>
   );
