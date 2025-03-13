4f9085b8b69a27522098842c93d1c01b0fabb560
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-13 21:52:45 +0530

    Components

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,5 +1,10 @@
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <label className="sidebar-label-container">
+      <input type="radio" name="test" />
+      <span className="checkmark"></span>ALL
+    </label>
+  );
 };
 
 export default Input;
